53d3b1f09cda57601de45ccf088614ff797ed62d
Bin_Laptop <ethan.ramdeen64@gmail.com>
2026-06-20 22:22:19 -0400

    oops

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,6 @@
 //! stb
 
 // stb_image_write.h
-const ImageWrite = @import("stb_image_write");
+pub const ImageWrite = @import("stb_image_write");
 // stb_image_write.h
-const TrueType = @import("stb_truetype");
+pub const TrueType = @import("stb_truetype");
